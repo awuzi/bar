9427068bf677e82e60286d7ff96e73625eebf227
Yahia <yahia@chefing.fr>
2021-02-17 12:05:55 +0100

    init Stats & Client entities

--- src/DataFixtures/StatisticFixtures.php
+++ src/DataFixtures/StatisticFixtures.php
@@ -1,19 +1,17 @@
 <?php
+
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 
-// Vos entités
-use App\Entity\Client;
-use App\Entity\Statistic;
-
 class StatisticFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        // TODO FIXTURES
+
+
     }
 
     public function getDependencies(): array
---
+++ src/Entity/Statistic.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StatisticRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StatisticRepository::class)
+ */
+class Statistic
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $score;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+}
